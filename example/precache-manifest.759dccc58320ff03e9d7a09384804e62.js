fc039b7cde7390fa3a23b3f45e404f35b41d03e7
Michael Talbot <mike.talbot@alcumusgroup.com>
2020-06-30 18:51:49 +0100

    Updates

---
+++ example/precache-manifest.759dccc58320ff03e9d7a09384804e62.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c3178da01aced3ef00f46a171f00783b",
+    "url": "./index.html"
+  },
+  {
+    "revision": "95ced6f2ce285599e133",
+    "url": "./static/css/main.baf7246d.chunk.css"
+  },
+  {
+    "revision": "2548fcc9bf3a8cd15842",
+    "url": "./static/js/2.9e5052f7.chunk.js"
+  },
+  {
+    "revision": "e8c255fadf9ba37b6a17971aabe4960c",
+    "url": "./static/js/2.9e5052f7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "95ced6f2ce285599e133",
+    "url": "./static/js/main.5eb87a3e.chunk.js"
+  },
+  {
+    "revision": "6fce53c7c7713ebf61712cc2929746fa",
+    "url": "./static/js/main.5eb87a3e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d86de88149d057ae2ded",
+    "url": "./static/js/runtime-main.1b7e62e6.js"
+  }
+]);
