468e2079b0890d7eef6e955c744aaebde0b85021
Michael Talbot <mike.talbot@alcumusgroup.com>
2020-07-03 23:14:45 +0100

    Updates

--- example/precache-manifest.67c2fea65fabad071bd86fda8d845ab7.js
+++ example/precache-manifest.67c2fea65fabad071bd86fda8d845ab7.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "e30f080fee4516390d88d91e22f00c4a",
+    "revision": "2e5509cbdbd780045cc103ba8df12b6b",
     "url": "./index.html"
   },
   {
-    "revision": "1b9dd3121df8ae0a256d",
+    "revision": "d360a05e087d1c9c78e6",
     "url": "./static/css/main.baf7246d.chunk.css"
   },
   {
@@ -16,12 +16,12 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "./static/js/2.9e5052f7.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "1b9dd3121df8ae0a256d",
-    "url": "./static/js/main.1f846eed.chunk.js"
+    "revision": "d360a05e087d1c9c78e6",
+    "url": "./static/js/main.94d2299e.chunk.js"
   },
   {
     "revision": "6fce53c7c7713ebf61712cc2929746fa",
-    "url": "./static/js/main.1f846eed.chunk.js.LICENSE.txt"
+    "url": "./static/js/main.94d2299e.chunk.js.LICENSE.txt"
   },
   {
     "revision": "d86de88149d057ae2ded",
--- example/service-worker.js
+++ example/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.8aa5b77df647c5b0cf9058bc68167f0b.js"
+  "./precache-manifest.67c2fea65fabad071bd86fda8d845ab7.js"
 );
 
 self.addEventListener('message', (event) => {
